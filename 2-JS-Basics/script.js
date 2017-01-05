e58a3ece8d4880fa590464970eba8f7b84fbc328
Javier Aguila <sjaca10@gmail.com>
2017-01-04 18:26:23 -0600

    Lecture: variables

---
+++ 2-JS-Basics/script.js
@@ -0,0 +1,16 @@
+// console.log('Hello world!');
+
+// Lecture: variables
+var name = 'John';
+console.log(name);
+
+var lastName = 'Smith';
+console.log(lastName);
+
+var age = 26;
+console.log(age);
+
+var fullAge = true;
+console.log(fullAge);
+
+
