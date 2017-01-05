a01e987e6be8af3b34c0f74ffee375144c7ed907
Javier Aguila <sjaca10@gmail.com>
2017-01-04 18:26:47 -0600

    Lectures: variables two

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -1,7 +1,8 @@
 // console.log('Hello world!');
 
 // Lecture: variables
-var name = 'John';
+/*
+var name = 'John'; 
 console.log(name);
 
 var lastName = 'Smith';
@@ -12,5 +13,31 @@ console.log(age);
 
 var fullAge = true;
 console.log(fullAge);
+*/
 
+// Lecture: variables two 
+var name = 'John';
+var age = 26;
 
+// console.log(name + age);
+// console.log(age + age);
+
+var job, isMarried;
+
+// console.log(job, isMarried);
+
+job = 'Teacher';
+isMarried = false;
+
+console.log(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried + '.');
+
+age = 'thirty six';
+job = 'driver';
+
+console.log(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried + '.');
+
+var lastName = prompt('What is the last name?');
+
+console.log(lastName);
+
+alert(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried + '.');
