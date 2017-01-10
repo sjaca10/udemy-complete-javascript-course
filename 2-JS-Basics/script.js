58c01e4e200158f84e63556e5e9d89930081030c
Javier Aguila <sjaca10@gmail.com>
2017-01-09 18:48:19 -0600

    Code Challenge 2

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -395,18 +395,16 @@ console.log(mike);
 //////////////////////////////////
 // Lecture: loops and iteration //
 //////////////////////////////////
-
+/*
 for (var i = 0; i < 10; i++) {
     console.log(i);
 }
 
-/*
-0, true, print 0, update 0 to 1
-1, true, print 1, update 1 to 2
-...
-9, true, print 9, update 9 to 10
-10, false, end loop
-*/
+// 0, true, print 0, update 0 to 1
+// 1, true, print 1, update 1 to 2
+// ...
+// 9, true, print 9, update 9 to 10
+//10, false, end loop
 
 var names = [
     'John',
@@ -449,4 +447,56 @@ for (var i = 0; i <= 5; i++) {
     }
     
     console.log(i);
+}
+*/
+
+////////////////////////
+// CODING CHALLENGE 2 //
+////////////////////////
+
+/*
+1. Create an array with some years where persons were born
+2. Create an empty array
+3. Use a loop to fill the array with the ages of the persons
+4. Use another loop to log into the console whether each person is of full age
+(18 or older), as well as their age
+5. Finally, create a function called printFullAge which receives the vector
+of two years an argument, executes the steps 2, 3 and 4 and returns a vector
+of true/false boolean values: true if the person is of full age (>= 18 years)
+and false if not (< 18 years)
+6. Call the function with two different vectors and store the results in two
+variables: full_1 and full_2
+
+Example input: [1965, 2008, 1992]
+Example output: [true, false, true]
+
+Hint: you can use a loop not only to read from an array, like y[i], but also
+to set values in an array, like y [i] = ... You can also use the specific
+array methods.
+*/
+
+function calculateAge(yearOfBirth) {
+    return new Date().getFullYear() - yearOfBirth;
+} 
+
+function calculateAges(yearsOfBirth) {
+    var result = [];
+    for (var i = 0; i < yearsOfBirth.length; i++) {
+        result[i] = calculateAge(yearsOfBirth[i]);
+    }
+    return result;
+}
+
+function isFullAge(years) {
+    var fullAge = 18, fullAges = [];
+    for (var i = 0; i < years.length; i++) {
+        fullAges[i] = years[i] >= 18 ? true : false;
+    }
+    return fullAges;
+}
+
+function printFullAge(years) {
+    var ages = calculateAges(years);
+    var fullAges = isFullAge(ages);
+    console.log(fullAges);
 }
