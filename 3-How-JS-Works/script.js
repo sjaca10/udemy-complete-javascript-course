78c1119ffbe9aa1547ca01a453f398f1f913efd3
Javier Aguila <sjaca10@gmail.com>
2017-01-11 11:42:33 -0600

    Lecture: this keyword

--- 3-How-JS-Works/script.js
+++ 3-How-JS-Works/script.js
@@ -43,7 +43,7 @@ first();
 function first() {
     var b = 'Hi!';
     second();
-    
+
     function second() {
         var c = 'Hey!';
         console.log(a + b + c);
@@ -51,14 +51,14 @@ function first() {
 }
 */
 // Example to show the difference between execution stack and scope chain
-
+/*
 var a = 'Hello!'
 first();
 
 function first() {
     var b = 'Hi!';
     second();
-    
+
     function second() {
         var c = 'Hey!';
         third();
@@ -68,5 +68,44 @@ function first() {
 function third() {
     var d = 'John';
     // console.log(c);
-    // Reference error because 
+    // Reference error because
 }
+*/
+
+///////////////////////////
+// Lecture: this keyword //
+///////////////////////////
+
+console.log(this);
+
+calculateAge(1985);
+
+function calculateAge(yearOfBirth) {
+    console.log(new Date().getFullYear() - yearOfBirth);
+    console.log(this);
+}
+
+var john = {
+    name: 'John',
+    yearOfBirth: 1990,
+    calculateAge: function() {
+        console.log(this);
+        console.log(new Date().getFullYear() - this.yearOfBirth);
+
+        function innerFunction() {
+            console.log(this);
+        }
+        innerFunction();
+    }
+}
+
+john.calculateAge();
+
+var mike = {
+    name: 'Mike',
+    yearOfBirth: 1984,
+};
+
+// Method Borrowing
+mike.calculateAge = john.calculateAge;
+mike.calculateAge();
