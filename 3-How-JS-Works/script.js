6cc532fbd1d8f7b045883c7ce0b64cd4542198a9
Javier Aguila <sjaca10@gmail.com>
2017-01-11 10:51:29 -0600

    Lecture: scoping

--- 3-How-JS-Works/script.js
+++ 3-How-JS-Works/script.js
@@ -1,7 +1,7 @@
 ///////////////////////
 // Lecture: Hoisting //
 ///////////////////////
-
+/*
 calculateAge(1965);
 
 function calculateAge(yearOfBirth) {
@@ -31,3 +31,42 @@ function foo() {
 
 foo();
 console.log(age);
+*/
+
+//////////////////////
+// Lecture: Scoping //
+//////////////////////
+/*
+var a = 'Hello!';
+first();
+
+function first() {
+    var b = 'Hi!';
+    second();
+    
+    function second() {
+        var c = 'Hey!';
+        console.log(a + b + c);
+    }
+}
+*/
+// Example to show the difference between execution stack and scope chain
+
+var a = 'Hello!'
+first();
+
+function first() {
+    var b = 'Hi!';
+    second();
+    
+    function second() {
+        var c = 'Hey!';
+        third();
+    }
+}
+
+function third() {
+    var d = 'John';
+    // console.log(c);
+    // Reference error because 
+}
