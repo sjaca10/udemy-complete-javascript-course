ad569bce2e2020e2096428b1cfec3e37d154617a
Javier Aguila <sjaca10@gmail.com>
2017-01-16 13:26:32 -0600

    Lecture: closures

--- 5-Advanced-JS/script.js
+++ 5-Advanced-JS/script.js
@@ -193,7 +193,7 @@ function game() {
 
 game();
 */
-
+/*
 // IIFE pattern. Parses understand this function declartion as expression and
 // calling the function
 (function () {
@@ -207,3 +207,44 @@ game();
     var score = Math.random() * 10;
     console.log(score >= 5);
 })(3);
+*/
+
+///////////////////////
+// Lecture: closures //
+///////////////////////
+
+function retirement (retirementAge) {
+    var a = ' years left until retirement.'
+    return function (yearOfBirth) {
+        var age = new Date().getFullYear() - yearOfBirth;
+        console.log((retirementAge - age) + a);
+    }
+}
+
+var retirementUS = retirement(66);
+retirementUS(1990);
+
+var retirementGermany = retirement(65);
+retirementGermany(1990);
+
+var retirmentIceland = retirement(67);
+retirmentIceland(1990);
+
+// retirement(66)(1990);
+
+// Closure challenge
+function interviewQuestion (job) {
+    return function (name) {
+        if (job === 'designer') {
+            console.log(name + ', can you please explain what UX design is?');
+        } else if (job === 'teacher') {
+            console.log('What subject do you teach, ' + name + '?');
+        } else {
+            console.log('Hello ' + name + ', what do you do?');
+        }
+    }
+}
+
+interviewQuestion('designer')('John');
+interviewQuestion('teacher')('Mike');
+interviewQuestion('other')('Jane');
