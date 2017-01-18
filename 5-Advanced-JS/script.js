71eacde0df086a9dd595e93ca5ca8444a30537cb
Javier Aguila <sjaca10@gmail.com>
2017-01-18 12:28:18 -0600

    Coding challenge: expert level

--- 5-Advanced-JS/script.js
+++ 5-Advanced-JS/script.js
@@ -486,12 +486,24 @@ function nextQuestion(questions) {
         }
     }
 
-    Question.prototype.checkAnswer = function(answer) {
+    Question.prototype.checkAnswer = function(answer, callback) {
+        var sc;
+
         if (answer === this.correct) {
             console.log('Correct answer!');
+            sc = callback(true);
         } else {
             console.log('Wrong answer. Try again!');
+
+            sc = callback(false);
         }
+
+        this.displayScore(sc);
+    }
+
+    Question.prototype.displayScore = function(score) {
+        console.log('Your current score is ' + score);
+        console.log('------------------------------')
     }
 
     var q1 = new Question('Is JavaScript the coolest programming language in the world?',
@@ -508,16 +520,29 @@ function nextQuestion(questions) {
 
     var questions = [q1, q2, q3];
 
+    function score() {
+        var score = 0;
+        return function(correct) {
+            if (correct) {
+                score++;
+            }
+            return score;
+        }
+    }
+
+    var keepScore = score();
+
     function nextQuestion() {
         var n = Math.floor(Math.random() * questions.length);
 
         questions[n].displayQuestion();
 
-        var answer = parseInt(prompt('Please selecte the correct answer'));
+        var answer = prompt('Please selecte the correct answer');
 
-        questions[n].checkAnswer(answer);
-
-        nextQuestion();
+        if (answer !== 'exit') {
+            questions[n].checkAnswer(parseInt(answer), keepScore);
+            nextQuestion();
+        }
     }
 
     nextQuestion();
